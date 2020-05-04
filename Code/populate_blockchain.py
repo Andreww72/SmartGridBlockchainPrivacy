f32d37fae52f9c92131b9a20f5ce3a5c99d172b7
Andrew Mather <andreww.mather@gmail.com>
2020-05-04 21:02:32 +1000

    Initial energy data wrangler and blockchain populating

--- Code/populate_blockchain.py
+++ Code/populate_blockchain.py
@@ -7,50 +7,104 @@ Wrangle the energy data into a format resembling what would be available publicl
 
 import os
 import sys
+import math
 
 import pandas as pd
 import numpy as np
+import hashlib
 
-years = ['Solar30minData_1Jul10-30Jun11', 'Solar30minData_1Jul11-30Jun12', 'Solar30minData_1Jul12-30Jun13']
-cols = ['Block', 'Tid', 'P_Tid', 'Timestamp', 'Type', 'Amount', 'Output', 'PK', 'Sign']
+#years = ['Solar30minData_1Jul10-30Jun11', 'Solar30minData_1Jul11-30Jun12', 'Solar30minData_1Jul12-30Jun13']
+years = ['Tester']
 block_size = 10
+
+
+def create_hash(hash_string):
+    sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()[0:8]
+    return sha_signature
+
 
 os.chdir('../EnergyData/MainData/')
 for year in years:
     # Load original energy data
     energy_data = pd.read_csv(year + '.csv', header=0)
+    energy_data_header = energy_data.columns.values.tolist()
 
-    transactions_list = []
-    block_transactions = 1
-    block_count = 1
+    #################################
+    # First, wrangle this idiotic Ausgrid data which used timestamps across columns, into rows using datetime.
+    wrangled_cols = ['Customer', 'Capacity', 'Timestamp', 'Type', 'Amount']
+    wrangled_list = []
+    first_kwh_col = 5
 
-    # Loop on each entry in energy data (this doesn't quite work cause want blockchain in timestamp order)
+    # Loop on input data, wrangling cols into rows of datetime.
     for index, row in energy_data.iterrows():
+        print(index)
+        # For each column of times during a day (48 half hour periods)
+        for i in range(48): # 0 to 47
 
-        transaction = [block_count, block_transactions, 0, 0, 0, 0, 0, 0, 0]
-        # Create PK for each transaction
-        # Structure of transaction: Tid | Prev Tid | timestamp | type | amount | output | PK | sign
-        # Tid is unique transaction identifier, hash of transaction content
-        # Prev Tid is identity of previous transaction in the same ledger
-        # Timestamp of when transaction packets generated in the database
-        # Type is GC, CL, or GG
-        # Amount is kWH amount
-        # Output is hash of PK device use in next transaction.
-        # PK of transaction generator and corresponding signature
-        # If data, transaction generator signs hash of data in sign field. Otherwise hash of transaction is signed.
+            # If amount used/generated for consumer, type, and time period is 0 then skip
+            if math.ceil(float(kwh_amount := row[first_kwh_col + i])) <= 0:
+                break
 
-        transactions_list.append(transaction)
+            datetime = f"{row['Date']} {energy_data_header[first_kwh_col + i]}"
+            eng_type = row['Consumption Category']
 
-        # 10 transactions per block
-        block_transactions += 1
-        if block_transactions > block_size:
+            wrangled_list.append([
+                row['Customer'],
+                row['Generator Capacity'],
+                datetime,
+                eng_type,
+                kwh_amount,
+            ])
+
+    wrangled_data = pd.DataFrame(wrangled_list, columns=wrangled_cols)
+    wrangled_data.sort_values(by=['Timestamp'], inplace=True)
+
+    # Save wrangled data as csv
+    os.chdir('../Blockchained/')
+    wrangled_data.to_csv(year + '_wrangled.csv', index=False)
+
+    #################################
+    # Second, populate the blockchain
+    blockchain_cols = ['Customer', 'Capacity', 'Block', 'Tid', 'P_Tid', 'Timestamp', 'Type', 'Amount', 'PK']
+    transaction_list = []
+    next_pk = 'PK'
+    block_count = 1
+    block_transacts = 1
+    prev_hash = ''
+
+    # Loop on wrangled data to create blockchain transaction format.
+    for index, row in wrangled_data.iterrows():
+        print(index)
+
+        # Structure of transaction: Block | Tid | Prev Tid | timestamp | type | amount | PK
+        datetime = row['Timestamp']
+        eng_type = row['Type']
+        kwh_amount = row['Amount']
+        curr_hash = create_hash(''.join([datetime, eng_type, str(kwh_amount)]))
+
+        transaction_list.append([
+            row['Customer'],
+            row['Capacity'],
+            block_count,
+            curr_hash,
+            prev_hash,
+            datetime,
+            eng_type,
+            kwh_amount,
+            next_pk
+        ])
+
+        # Updates for next loop
+        prev_hash = curr_hash
+        block_transacts += 1
+        if block_transacts > block_size:
             block_count += 1
-            block_transactions = 1
+            block_transacts = 1
 
     # Single node acts as miner and collects all home transactions.
-    blockchain_data = pd.DataFrame(transactions_list, columns=cols)
+    blockchain_data = pd.DataFrame(transaction_list, columns=blockchain_cols)
+    #blockchain_data.sort_values(by=['Timestamp'], inplace=True)
 
     # Save resulting blockchain as csv
-    os.chdir('../Blockchained/')
     blockchain_data.to_csv(year + '_blockchain.csv', index=False)
     os.chdir('../MainData/')
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Smart-Grid-Blockchain-Privacy
+﻿# Smart-Grid-Blockchain-Privacy
 
 Preserving User Anonymity in Smart Grid Blockchain Transactions
 
@@ -7,3 +7,4 @@ Engineering Computer and Software Systems Honours Thesis
 This project aims to contribute to the study of user anonymity in an Internet of Things Blockchain. The project will explore methods to deanonymise users with machine learning. This analysis will include stored Blockchain transactions and off-the-chain data. Section 1 will investigate the topic and surrounding literature. We will then identify the project’s research problem, objectives, questions, scope, and propose a preliminary methodology to complete the project. The aim is to determine how effective machine learning is to deanonymise smart grid Blockchain users. This should highlight long-term anonymity risks of Blockchain by analysing permanent transaction data. The approach will first, source energy grid data and populate a Blockchain. Second, apply machine learning analysis on the Blockchain to link users and transactions. Third, incorporate off-the-chain data such as climate, zone distribution, and outages. Last, the project will investigate and measure methods to improve user privacy. Project outcomes will include a better understanding of user privacy risks in the discussed scenario.
 
 Update dependencies: pipreqs /path/to/project --force
+Run scripts with Powershell or Terminal, avoid an IDE as too slow.
