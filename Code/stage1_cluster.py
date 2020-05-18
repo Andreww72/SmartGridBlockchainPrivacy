bf7c414813bfa63376e758709c367a997bdc059f
Andrew Mather <andreww.mather@gmail.com>
2020-05-18 21:58:06 +1000

    Stage 1 cluster euclidean distance monthly

--- Code/stage1_cluster.py
+++ Code/stage1_cluster.py
@@ -10,11 +10,10 @@ This treats each household as separate ledgers
 Clustering methods
     Comparing datasets themselves against each other
     i) Euclidean distance, ii) LTSM, iii) Cointegration analysis, iv) Clustering
-    Clustering: https://petolau.github.io/Multiple-data-streams-clustering-in-r/
-    https://www.researchgate.net/publication/322011057_Time_Series_Analysis_for_Spatial_Node_Selection_in_Environment_Monitoring_Sensor_Networks
-    LSTM, Cointegration analysis
 
-    https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
+    ii)  https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
+    iii) https://www.researchgate.net/publication/322011057_Time_Series_Analysis_for_Spatial_Node_Selection_in_Environment_Monitoring_Sensor_Networks
+    iv)  https://petolau.github.io/Multiple-data-streams-clustering-in-r/
 """
 
 import os
@@ -22,16 +21,13 @@ import json
 
 import pandas as pd
 import numpy as np
-from sklearn import cluster
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 
-os.chdir("../BlockchainData/")
+os.chdir("../BlockchainData/Monthly")
 
 # Euclidean distance
 results = {}
 results_list = []
-comparison_vector = np.zeros(78913-1)
+comparison_vector = np.zeros(108)
 
 for num in range(300):
     if not num == 2-1:
@@ -42,39 +38,4 @@ for num in range(300):
         results[num+1] = euc_dist
         results_list.append(euc_dist)
 
-json.dump(results, open("results.json", "w"), indent=4)
-
-# Simple cluster of Euclidean distance results for visualisation
-np_array = np.array(results_list)
-k_means = KMeans(algorithm='auto', copy_x=True, init='k-means++', max_iter=300,
-                 n_clusters=3, n_init=10, random_state=0, tol=0.0001, verbose=0)
-
-quotient_2d = np_array.reshape(-1, 1)
-k_means.fit(quotient_2d)
-
-times = []
-for n in range(300):
-    times.append(n)
-
-colors = ['r', 'g', 'b']
-centroids = k_means.cluster_centers_
-for n, y in enumerate(centroids):
-    plt.plot(1, y, marker='x', color=colors[n], ms=10)
-plt.title('Kmeans cluster centroids')
-
-Z = k_means.predict(quotient_2d)
-
-n_clusters = 3
-for n in range(n_clusters):
-    # Filter data points to plot each in turn.
-    ys = np_array[Z == n]
-    xs = times[Z == n]
-
-    plt.scatter(xs, ys, color=colors[n])
-
-plt.title("Points by cluster")
-
-# DTW
-
-# Fancy cluster
-# Might use R for this
+json.dump(results, open("0_results.json", "w"), indent=4)
