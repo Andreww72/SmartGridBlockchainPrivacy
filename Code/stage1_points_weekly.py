06c1ff6d029d2a041192ff32f84b72298ee8a637
Andrew Mather <andreww.mather@gmail.com>
2020-05-25 13:09:26 +1000

    KNN and KMeans

--- Code/stage1_points_weekly.py
+++ Code/stage1_points_weekly.py
@@ -5,7 +5,7 @@
 ML analysis
 1a) Grid data only, informed attacker: classification
 
-Use: python ./stage1_classify_weekly.py [case] [MLP] [LSTM]
+Use: python ./stage1_points_weekly.py [case] [MLP] [KNN] [KMS]
 Use a 0 for worst case, 1 for best case for case argument
 Use a 1 or 0 indicator for MLP and LSTM arguments
 
@@ -29,24 +29,22 @@ import os
 import sys
 import multiprocessing
 import pandas as pd
+import numpy as np
 
-# Classify MLP
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-
-# Classify KNN
-
-
-case = None
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 ###################################
 ##         Preprocessing         ##
 ###################################
-def preprocessing(strip_zeros=False):
+def preprocessing(case=1, strip_zeros=False):
     print("Preprocessing stage 1 weekly data")
 
     weekly_data = pd.read_csv('0_1a_combined_weekly.csv', header=0)
@@ -57,18 +55,18 @@ def preprocessing(strip_zeros=False):
     weekly_data['Timestamp'] = pd.to_datetime(weekly_data['Timestamp'], dayfirst=True)
     weekly_data['Timestamp'] = (weekly_data.Timestamp - pd.to_datetime('1970-01-01')).dt.total_seconds()
 
-    if int(case) == 0:
+    if case == 0:
         print("Preprocessing data for worst case")
         # Drop the PK and hash information
         weekly_data.drop(['Hash', 'PHash', 'PK'], axis=1)
         # Structure: Customer | Postcode | Generator | Timestamp | Type | Amount
-    elif int(case) == 1:
+    elif case == 1:
         print("Preprocessing data for best case")
         # Don't remove anything lol
         # Structure: Customer | Postcode | Generator | Hash | PHash | PK | Timestamp | Type | Amount
     else:
         print("Invalid case selected")
-        print("Invalid usage: python ./stage1_classify_weekly.py [case] [MLP] [LSTM]")
+        print("Invalid usage: python ./stage1_points_weekly.py [case] [MLP] [KNN] [KMS]")
         print("Use a 0 for worst case, 1 for best case for case argument")
         print("Use a 1 or 0 indicator for MLP and LSTM arguments")
 
@@ -105,13 +103,15 @@ def preprocessing(strip_zeros=False):
 ###################################
 ##         Classify MLP          ##
 ###################################
-def mlp(customer, postcode):
+def mlp(case, customer, postcode):
+    preprocessing(case, True)
+
     if customer:
         print("Applying MLP neural network for customer number")
         mlp_num = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=500)
         mlp_num.fit(X_train_num, Y_train_num)
         mlp_predictions_num = mlp_num.predict(X_test_num)
-        print("NN number weekly accuracy: ", accuracy_score(Y_test_num, mlp_predictions_num, normalize=True))
+        print("NN number weekly accuracy: ", accuracy_score(Y_test_num, mlp_predictions_num))
 
     if postcode:
         print("Applying MLP neural network for postcode")
@@ -122,47 +122,116 @@ def mlp(customer, postcode):
 
 
 ###################################
-##         Classify LSTM         ##
+##         Classify KNN          ##
 ###################################
-def knn():
-    preprocessing(False)
+def knn(case, customer, postcode):
+    ks = [1, 3, 5, 10, 20, 50]
+    results_num = []
+    results_post = []
+
+    print(f"KNN with k values: {ks}")
+    preprocessing(case, False)
+
+    if customer:
+        print("Applying KNN for customer")
+        for k in ks:
+            knn_num = KNeighborsClassifier(n_neighbors=k)
+            knn_num.fit(X_train_num, Y_train_num)
+            knn_predictions_num = knn_num.predict(X_test_num)
+            result = accuracy_score(Y_test_num, knn_predictions_num)
+            results_num.append(result)
+        best_k = ks[results_num.index(max(results_num))]
+        print(f"Best KNN number weekly accuracy (k={best_k}:", max(results_num))
+
+    if postcode:
+        print("Applying KNN for postcode")
+        for k in ks:
+            knn_post = KNeighborsClassifier(n_neighbors=k)
+            knn_post.fit(X_train_post, Y_train_post)
+            knn_predictions_post = knn_post.predict(X_test_post)
+            result = accuracy_score(Y_test_post, knn_predictions_post)
+            results_post.append(result)
+        best_k = ks[results_post.index(max(results_post))]
+        print(f"KNN postcode weekly accuracy (k={best_k}:", max(results_post))
+
+
+def kms(case):
+    preprocessing(case, False)
+
+    clusters = 100
+    reduced_data = PCA(n_components=2).fit_transform(X_train_num)
+    kmeans = KMeans(init='k-means++', n_clusters=clusters, n_init=10)
+    kmeans.fit(reduced_data)
+
+    # Step size of the mesh. Decrease to increase the quality of the VQ.
+    h = .02  # point in the mesh [x_min, x_max]x[y_min, y_max].
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Obtain labels for each point in mesh. Use last trained model.
+    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure(1)
+    plt.clf()
+    plt.imshow(Z, interpolation='nearest',
+               extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+               aspect='auto', origin='lower')
+
+    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+    # Plot the centroids as a red .
+    centroids = kmeans.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='.', s=169, linewidths=2,
+                color='r', zorder=10)
+    plt.title('K-means clustering (PCA-reduced data)\n'
+              'Centroids are marked with red dot')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
 
 
 if __name__ == '__main__':
     # Check usage
-    if not len(sys.argv) == 4:
-        print("Invalid usage: python ./stage1_classify_weekly.py [case] [MLP] [LSTM]")
+    if not len(sys.argv) == 5:
+        print("Invalid usage: python ./stage1_points_weekly.py [case] [MLP] [KNN] [KMS]")
         print("Use a 0 for worst case, 1 for best case for case argument")
         print("Use a 1 or 0 indicator for MLP and LSTM arguments")
         exit()
 
-    case = sys.argv[1]
+    case = int(sys.argv[1])
 
     os.chdir("../BlockchainData/Weekly")
 
     if int(sys.argv[2]):
-
         print("Classifying stage 1 weekly data with MLP neural network")
-        preprocessing(True)
 
         print("Creating 2 processes for MLP analysis")
         processes = [
             multiprocessing.Process(target=mlp,
                                     name="Process Customer",
-                                    args=(True, False)),
+                                    args=(case, True, False)),
             multiprocessing.Process(target=mlp,
                                     name="Process Postcode",
-                                    args=(False, True))]
+                                    args=(case, False, True))]
         for p in processes:
             p.start()
-
-        # Wait for completion
         for p in processes:
             p.join()
 
     if int(sys.argv[3]):
-        print("Classifying stage 1 weekly data with LSTM")
-        knn()
+        print("Classifying stage 1 weekly data with kNN")
+        knn(case, True, True)
+
+    if int(sys.argv[4]):
+        print("KMEANS CLUSTER")
+        kms(case)
 
 # from sklearn.metrics import classification_report, confusion_matrix
 # print(confusion_matrix(Y_test_num, predictions_num_nn))
